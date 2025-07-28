a3180c88d56e193cda712b90481009063b52e7ce
jayantpatel06 <kbzbou7177@gmail.com>
2025-07-29 00:37:25 +0530

    Enhance interface and finished project 2

--- 2-BMI_Calculator/script.js
+++ 2-BMI_Calculator/script.js
@@ -1,4 +1,20 @@
 const form = document.querySelector('form')
+const result = document.querySelector('#results')
 
+form.addEventListener('submit', function(e){
+    e.preventDefault()
 
-form.addEventListener('submit', )
+    const height = parseInt(document.querySelector('#height').value)
+    const weight = parseInt(document.querySelector('#weight').value)
+
+    if(height==='' || height <0 || isNaN(height)){
+        result.innerHTML = "Please give a valid Height"
+    }
+    else if(weight==='' || weight <0 || isNaN(weight)){
+        result.innerHTML = "Please give a valid Weight"
+    }
+    else{
+        const bmi = (weight/((height*height)/10000)).toFixed(2)
+        result.innerHTML= `BMI : <span>${bmi}</span>`
+    } 
+})
